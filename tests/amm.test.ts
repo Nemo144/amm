f0297c58f4c9de288828663ca419b44df04968a9
Nnamdi Okeke <onnamdialex@gmail.com>
2026-03-16 19:03:55 +0100

    :white_check_mark: Writing test for the amm contract | wip
    defined the helper functions for the test to make things easier

--- tests/amm.test.ts
+++ tests/amm.test.ts
@@ -1,20 +1,80 @@
-import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { describe, expect, it, beforeEach } from "vitest";
 
 const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+const deployer = accounts.get("deployer")!;
+const alice = accounts.get("wallet_1")!;
+const bob = accounts.get("wallet_2")!;
+const charlie = accounts.get("wallet_3")!;
 
-/*
-The test below is an example. To learn more, read the testing documentation here:
-https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
+//assigning the two mock token addresses to variables for easier use
+const mockTokenOne = Cl.contractPrincipal(deployer, "mock-token");
+const mockTokenTwo = Cl.contractPrincipal(deployer, "mock-token-2");
 
-describe("example tests", () => {
-it("ensures simnet is well initialised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-});
+//setting up few helpers to make it easier to call the four public functions and the helper function `get-pool-id`
 
-// it("shows an example", () => {
-//   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-//   expect(result).toBeUint(0);
-// });
-});
+//create pool helper function
+function createPool() {
+  return simnet.callPublicFn(
+    "amm",
+    "create-pool",
+    [mockTokenOne, mockTokenTwo, Cl.uint(500)],
+    alice,
+  );
+}
+
+function addLiquidity(account: string, amount0: number, amount1: number) {
+  return simnet.callPublicFn(
+    "amm",
+    "add-liquidity",
+    [
+      mockTokenOne,
+      mockTokenTwo,
+      Cl.uint(500),
+      Cl.uint(amount0),
+      Cl.uint(amount1),
+      Cl.uint(0),
+      Cl.uint(0),
+    ],
+    account,
+  );
+}
+
+function removeLiquidity(account: string, liquidity: number) {
+  return simnet.callPublicFn(
+    "amm",
+    "remove-liquidity",
+    [mockTokenOne, mockTokenTwo, Cl.uint(500), Cl.uint(liquidity)],
+    account,
+  );
+}
+
+function swap(account: string, inputAmount: number, zeroForOne: boolean) {
+  return simnet.callPublicFn(
+    "amm",
+    "swap",
+    [
+      mockTokenOne,
+      mockTokenTwo,
+      Cl.uint(500),
+      Cl.uint(inputAmount),
+      Cl.bool(zeroForOne),
+    ],
+    account,
+  );
+}
+
+function getPoolId() {
+  return simnet.callReadOnlyFn(
+    "amm",
+    "get-pool-id",
+    [
+      Cl.tuple({
+        "token-0": mockTokenOne,
+        "token-1": mockTokenTwo,
+        fee: Cl.uint(500),
+      }),
+    ],
+    alice,
+  );
+}
