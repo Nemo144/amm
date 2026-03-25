3d264b8b457d97b9c94a4ee6dac8bac3d21669ca
Nnamdi Okeke <onnamdialex@gmail.com>
2026-03-25 17:07:13 +0100

    :zap: Add the helper functions for the abbreviated address and transaction id

---
+++ frontend/lib/stx-utils.ts
@@ -0,0 +1,9 @@
+//define the abbreviated address helper function
+export async function abbreviateAddress(address: string) {
+  return `${address.substring(0, 5)}...${address.substring(36)}`;
+}
+
+//define the abbreviated transaction id helper function
+export async function abbreviateTxnId(txnId: string) {
+  return `${txnId.substring(0, 5)}...${txnId.substring(62)}`;
+}
